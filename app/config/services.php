1c102b32ad39bc854d8f8896387c62dcdde3d543
zeryzez <lolmitoman@gmail.com>
2025-09-23 15:06:13 +0200

    fix: Praticien service and update settings

--- app/config/services.php
+++ app/config/services.php
@@ -5,14 +5,14 @@ use toubilib\core\application\usecases\ServicePraticien;
 use toubilib\core\application\usecases\ServicePraticienInterface;
 
 return [
-    'pdo' => function($container) {
-        $settings = $container->get('settings');
-        $dsn = "pgsql:host={$settings['db.host']};dbname={$settings['db.name']}";
-        return new PDO($dsn, $settings['db.user'], $settings['db.pass']);
+    'pdo.praticien' => function($container) {
+        $settings = $container->get('db.praticien');
+        $dsn = "pgsql:host={$settings['db.praticien.host']};dbname={$settings['db.praticien.name']}";
+        return new PDO($dsn, $settings['db.praticien.user'], $settings['db.praticien.pass']);
     },
 
     PraticienRepositoryInterface::class => function($container) {
-        return new PDOPraticienRepository($container->get('pdo'));
+        return new PDOPraticienRepository($container->get('pdo.praticien'));
     },
 
     ServicePraticienInterface::class => function($container) {
--- app/config/settings.php
+++ app/config/settings.php
@@ -1,7 +1,22 @@
 <?php
 return [
-    'db.host' => 'localhost',
-    'db.name' => 'toubiauth',
-    'db.user' => 'toubiauth',
-    'db.pass' => 'toubiauth',
+    'db.praticien.host' => 'toubiprat.db',
+    'db.praticien.name' => 'toubiprat',
+    'db.praticien.user' => 'toubiprat',
+    'db.praticien.pass' => 'toubiprat',
+
+    'db.patient.host' => 'toubipat.db',
+    'db.patient.name' => 'toubipat',
+    'db.patient.user' => 'toubipat',
+    'db.patient.pass' => 'toubipat',
+
+    'db.rdv.host' => 'toubirdv.db',
+    'db.rdv.name' => 'toubirdv',
+    'db.rdv.user' => 'toubirdv',
+    'db.rdv.pass' => 'toubirdv',
+
+    'db.auth.host' => 'toubiauth.db',
+    'db.auth.name' => 'toubiauth',
+    'db.auth.user' => 'toubiauth',
+    'db.auth.pass' => 'toubiauth',
 ];
