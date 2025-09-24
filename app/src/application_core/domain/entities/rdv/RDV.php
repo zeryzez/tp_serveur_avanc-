bab1419ca07c10a7cab172c7bbe6d2f6d23f3ff6
Ryad <kaderdu10120@gmail.com>
2025-09-24 16:51:42 +0200

    méthode annuler de la classe rdv

--- app/src/application_core/domain/entities/rdv/RDV.php
+++ app/src/application_core/domain/entities/rdv/RDV.php
@@ -48,4 +48,20 @@ class RDV {
     public function getDuree(): int { return $this->duree; }
     public function getDateCreation(): string { return $this->date_creation; }
     public function getMotifVisite(): string { return $this->motif_visite; }
+
+    public function annuler(): void {
+
+        if ($this->status === 0) {
+            throw new Exception("Le rendez-vous est déjà annulé.");
+        }
+
+        $now = new \DateTime();
+        $debut = new \DateTime($this->date_heure_debut);
+
+        if ($now > $debut) {
+            throw new Exception("Impossible d'annuler un rendez-vous dont la dâte est déjà passée.");
+        }
+
+        $this->status = 0; // Annulé
+    }
 }
