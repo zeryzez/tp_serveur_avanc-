3ea39cfacbf9cb55d87af887f65c5d406f2f1df3
Vivienhrm <vivienherrmann67350@gmail.com>
2025-09-26 14:50:17 +0200

    ajout de la méthode creerRendezVous à ServiceRdvInterface

--- app/src/application_core/application/ports/api/ServiceRdvInterface.php
+++ app/src/application_core/application/ports/api/ServiceRdvInterface.php
@@ -3,9 +3,12 @@
 namespace toubilib\core\application\ports\api;
 
 use toubilib\core\application\dto\RdvDTO;
+use toubilib\core\application\dto\InputRendezVousDTO;
 
 interface ServiceRdvInterface {
     public function getRdvById(string $id): ?RdvDTO;
     public function listerCreneauxOccupes(string $praticienId, string $dateDebut, string $dateFin): array;
+
+    public function creerRendezVous(InputRendezVousDTO $dto): void;
     public function annulerRendezVous(string $idRdv): void;
 }
