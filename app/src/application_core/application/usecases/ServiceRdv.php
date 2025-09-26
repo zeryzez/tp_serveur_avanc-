f74d30854314be38d87c54e7411396ce36dc8810
Vivienhrm <vivienherrmann67350@gmail.com>
2025-09-26 14:59:58 +0200

    implémenter creerRendezVous au serviceRendezVous

--- app/src/application_core/application/usecases/ServiceRdv.php
+++ app/src/application_core/application/usecases/ServiceRdv.php
@@ -3,6 +3,8 @@ namespace toubilib\core\application\usecases;
 
 use toubilib\core\application\ports\spi\repositoryInterfaces\RdvRepositoryInterface;
 use toubilib\core\application\dto\RdvDTO;
+use toubilib\core\application\dto\InputRendezVousDTO;
+use toubilib\core\domain\entities\rdv\RDV;
 use toubilib\core\application\ports\api\ServiceRdvInterface;
 
 class ServiceRdv implements ServiceRdvInterface {
@@ -44,6 +46,32 @@ class ServiceRdv implements ServiceRdvInterface {
                 $rdv->getMotifVisite()
             );
         }, $rdvs);
+    }
+
+    public function creerRendezVous(InputRendezVousDTO $dto): void {
+        $id = uniqid();
+        $dateDebut = new \DateTime($dto->date_heure_debut);
+        $dateFin = clone $dateDebut;
+        $dateFin->add(new \DateInterval('PT' . $dto->duree . 'M'));
+        $date_heure_fin = $dateFin->format('Y-m-d H:i:s');
+        $status = 1; // Assuming 1 for active/confirmed
+        $date_creation = date('Y-m-d H:i:s');
+        $patient_email = null;
+
+        $rdv = new RDV(
+            $id,
+            $dto->praticien_id,
+            $dto->patient_id,
+            $patient_email,
+            $dto->date_heure_debut,
+            $date_heure_fin,
+            $status,
+            $dto->duree,
+            $date_creation,
+            $dto->motif_visite
+        );
+
+        $this->rdvRepository->save($rdv);
     }
 
     public function annulerRendezVous(int $idRdv): void {
